5dc6c3b773487148290454da366231497b2704e3
majd02 <49454413+majd02@users.noreply.github.com>
2019-04-10 22:22:47 +0100

    Update and rename iniback2.h to back2.h

---
+++ back2.h
@@ -0,0 +1,23 @@
+# alpha-c
+#ifndef FICHIER_H
+#define FICHIER_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h> 
+#include <string.h>
+
+typedef struct BACKGROUND
+{ 
+SDL_Surface* fond[3];
+SDL_Rect position;
+
+}BACKGROUND ;
+
+void intialiser_back(BACKGROUND *back);
+void affichback(BACKGROUND *back,SDL_Surface *ecran);
+
+#endif
+
+
