2ff5ba21ddd95313f898c29b985e72403d9605a8
majd02 <49454413+majd02@users.noreply.github.com>
2019-04-10 22:10:17 +0100

    Create iniback2.h

---
+++ iniback2.h
@@ -0,0 +1,18 @@
+# alpha-c
+#include "fichier.h"
+
+void intialiser_back(BACKGROUND *back)
+{ 
+back->fond[0]=IMG_Load("fond.png");
+
+if(back->fond[0]==NULL)
+		exit(EXIT_FAILURE);
+
+back->position.x=0;
+back->position.y=0;
+
+
+
+
+}
+
